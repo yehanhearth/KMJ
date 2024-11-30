3978c2d939d85aca860fe89591819bd0db843ec3
Yehan Herath <yehan122@icloud.com>
2024-11-30 12:14:04 +0530

    FINE NAV

--- script.js
+++ script.js
@@ -2,4 +2,9 @@ const hamBurger = document.querySelector(".toggle-btn");
 
 hamBurger.addEventListener("click", function () {
   document.querySelector("#sidebar").classList.toggle("expand");
+
+  
+
+
+
 });
